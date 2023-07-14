b31ba81f915d4deaa33edcf84b3609594feda038
Morgan Bosc <morganbosc.music@gmail.com>
2023-07-14 14:47:40 +0200

    Change HTML

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 document.getElementById("login-button").addEventListener("click", function() {
   // Perform Spotify authentication process here
   // You'll need to use the Spotify Web API or the Spotify Web Playback SDK for this step
+  // Add your code here to handle the login button click event
 });
