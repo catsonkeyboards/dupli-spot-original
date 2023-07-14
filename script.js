af6f54cae08d7ff77df9fee9f7c73fe763eeb2bd
Morgan Bosc <morganbosc.music@gmail.com>
2023-07-14 15:25:21 +0200

    Spotify Authent
    We started to create our Spotify Authentication Process using the Spotify API. It runs locally.

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 // script.js
 document.getElementById("login-button").addEventListener("click", function() {
-  // Perform Spotify authentication process here
-  // You'll need to use the Spotify Web API or the Spotify Web Playback SDK for this step
-  // Add your code here to handle the login button click event
+  // Spotify authentication process
+  // Replace YOUR_CLIENT_ID with your actual Spotify API client ID
+  
+  // Define the Spotify authorization URL
+  const authUrl = `https://accounts.spotify.com/authorize?client_id=9fdba1a5111447ebad9b2213859f814a&response_type=token&redirect_uri=http://localhost:3000/callback`;
+  
+  // Open the authentication window in a new dialog
+  const authWindow = window.open(authUrl, "_blank", "width=500,height=600");
+
+  // Handle the callback from the authentication window
+  const handleCallback = (event) => {
+    // Check if the event originated from the authentication window
+    if (event.source === authWindow) {
+      // Extract the access token from the URL
+      const accessToken = event.data.access_token;
+      
+      // Perform further actions with the access token
+      // Add your code here to interact with the Spotify API using the access token
+      
+      // Close the authentication window
+      authWindow.close();
+      
+      // Remove the event listener
+      window.removeEventListener("message", handleCallback);
+    }
+  };
+  
+  // Listen for the callback message from the authentication window
+  window.addEventListener("message", handleCallback);
 });
