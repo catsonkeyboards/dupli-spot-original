903f8cb987e43f9be03af9bdc9d6098e715791c9
Morgan Bosc <morganbosc.music@gmail.com>
2023-07-14 18:22:55 +0200

    authen window size fix

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ document.getElementById("login-button").addEventListener("click", function() {
   // Define the Spotify authorization URL
   const authUrl = `https://accounts.spotify.com/authorize?client_id=9fdba1a5111447ebad9b2213859f814a&response_type=token&redirect_uri=http://localhost:3000/callback`;
   
-  // Open the authentication window in a new dialog
-  const authWindow = window.open(authUrl, "_blank", "width=500,height=600");
+  // Calculate the window size based on the content
+  const windowWidth = Math.min(window.innerWidth - 100, 500); // Adjust the subtracted value as needed
+  const windowHeight = Math.min(window.innerHeight - 100, 800); // Adjust the subtracted value as needed
+  
+  // Open the authentication window with the calculated size
+  const authWindow = window.open(authUrl, "_blank", `width=${windowWidth},height=${windowHeight}`);
 
   // Handle the callback from the authentication window
   const handleCallback = (event) => {
