eb9196cbda0f8503a64c2c673e05b5c2186336f0
Morgan Bosc <morganbosc.music@gmail.com>
2023-07-18 16:36:24 +0200

    fixes

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 // script.js
+let accessToken;
+
+function fetchPlaylists() {
+  // Fetch the playlists from the Spotify API
+  fetch('https://api.spotify.com/v1/me/playlists', {
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Log the playlist data to the console for now, you can update this later to display the playlists on the page
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
 document.getElementById("login-button").addEventListener("click", function() {
   // Spotify authentication process
   // Replace YOUR_CLIENT_ID with your actual Spotify API client ID
   
   // Define the Spotify authorization URL
-  const authUrl = `https://accounts.spotify.com/authorize?client_id=9fdba1a5111447ebad9b2213859f814a&response_type=token&redirect_uri=http://localhost:3000/callback`;
+  const authUrl = `https://accounts.spotify.com/authorize?client_id=9fdba1a5111447ebad9b2213859f814a&response_type=token&redirect_uri=http://localhost:5500/redirect.html`;
   
   // Calculate the window size based on the content
   const windowWidth = Math.min(window.innerWidth - 100, 500); // Adjust the subtracted value as needed
@@ -15,20 +34,18 @@ document.getElementById("login-button").addEventListener("click", function() {
 
   // Handle the callback from the authentication window
   const handleCallback = (event) => {
-    // Check if the event originated from the authentication window
-    if (event.source === authWindow) {
-      // Extract the access token from the URL
-      const accessToken = event.data.access_token;
+    // Save the access token to the accessToken variable
+    accessToken = event.data.access_token;
+
+    // Perform further actions with the access token
+    // Add your code here to interact with the Spotify API using the access token
       
-      // Perform further actions with the access token
-      // Add your code here to interact with the Spotify API using the access token
-      
-      // Close the authentication window
-      authWindow.close();
-      
-      // Remove the event listener
-      window.removeEventListener("message", handleCallback);
-    }
+    // Remove the event listener
+    window.removeEventListener("message", handleCallback);
+        
+    // Call fetchPlaylists after successful authentication
+    fetchPlaylists();
+
   };
   
   // Listen for the callback message from the authentication window
