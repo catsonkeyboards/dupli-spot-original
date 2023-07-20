b6456722d808d5417e8dd1850e6e19cf99368a23
Morgan Bosc <morganbosc.music@gmail.com>
2023-07-20 15:09:28 +0200

    buttons
    No pricing page, change to buttons, checkbox added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// script.js
 let accessToken;
 let offset = 0;
-const limit = 20;
+const limit = 40;
 let selectedPlaylists = [];
+let selectedDuplicates = [];
 
 function fetchPlaylists(offset = 0) {
   // Fetch the playlists from the Spotify API
@@ -11,79 +11,95 @@ function fetchPlaylists(offset = 0) {
       'Authorization': 'Bearer ' + accessToken
     }
   })
-  .then(response => response.json())
-  .then(data => {
-    // Log the playlist data to the console for now
-    console.log(data);
+    .then(response => response.json())
+    .then(data => {
+      // Log the playlist data to the console for now
+      console.log(data);
 
-    // Create an HTML string with the playlist data
-    let html = '';
-    data.items.forEach(playlist => {
-      html += `
-      <div>
-        <input type="checkbox" id="${playlist.id}" value="${playlist.id}" name="playlist">
-        <label for="${playlist.id}"><strong>${playlist.name}</strong> - ${playlist.tracks.total} tracks</label>
-      </div>`;
-    });
-
-    // Get the playlists section
-    const playlistsSection = document.getElementById('playlists');
-
-    // Append the HTML string to the playlists section
-    playlistsSection.innerHTML += html;
-
-    // Hide the playlists section
-    playlistsSection.style.display = 'none';
-
-    // Get the checkboxes and add event listeners
-    const checkboxes = document.getElementsByName('playlist');
-    checkboxes.forEach(checkbox => {
-      checkbox.addEventListener('change', (event) => {
-        if (event.target.checked) {
-          selectedPlaylists.push(event.target.value);
-        } else {
-          const index = selectedPlaylists.indexOf(event.target.value);
-          if (index > -1) {
-            selectedPlaylists.splice(index, 1);
-          }
-        }
-
-        // Call updateDuplicatesButtonState after a checkbox's state changes
-        updateDuplicatesButtonState();
+      // Create an HTML string with the playlist data
+      let html = '';
+      data.items.forEach(playlist => {
+        html += `
+        <div>
+          <input type="checkbox" id="${playlist.id}" value="${playlist.id}" name="playlist">
+          <label for="${playlist.id}"><strong>${playlist.name}</strong> - ${playlist.tracks.total} tracks</label>
+        </div>`;
       });
+
+      // Get the playlists section
+      const playlistsSection = document.getElementById('playlists');
+
+      // Append the HTML string to the playlists section
+      playlistsSection.innerHTML += html;
+
+      // Hide the playlists section
+      playlistsSection.style.display = 'none';
+
+      // Get the checkboxes and add event listeners
+      const checkboxes = document.getElementsByName('playlist');
+      checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', (event) => {
+          if (event.target.checked) {
+            selectedPlaylists.push(event.target.value);
+          } else {
+            const index = selectedPlaylists.indexOf(event.target.value);
+            if (index > -1) {
+              selectedPlaylists.splice(index, 1);
+            }
+          }
+
+          // Call updateDuplicatesButtonState after a checkbox's state changes
+          updateDuplicatesButtonState();
+        });
+      });
+
+      // Get the Load More button
+      const loadMoreButton = document.getElementById('load-more');
+
+      // If there are more playlists to fetch, show the Load More button. Otherwise, hide it.
+      if (data.items.length === limit) {
+        loadMoreButton.style.display = 'block';
+      } else {
+        loadMoreButton.style.display = 'none';
+      }
+
+      // Show the playlists section after fetching and appending the playlists
+      playlistsSection.style.display = 'block';
+    })
+    .catch(error => {
+      console.error('Error:', error);
     });
-
-    // Get the Load More button
-    const loadMoreButton = document.getElementById('load-more');
-
-    // If there are more playlists to fetch, show the Load More button. Otherwise, hide it.
-    if (data.items.length === limit) {
-      loadMoreButton.style.display = 'block';
-    } else {
-      loadMoreButton.style.display = 'none';
-    }
-
-    // Show the playlists section after fetching and appending the playlists
-    playlistsSection.style.display = 'block';
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
 }
 
+// Function to update the "Show Duplicates" button's state
 function updateDuplicatesButtonState() {
   const showDuplicatesButton = document.getElementById('show-duplicates');
-  if (selectedPlaylists.length === 2) {
+
+  if (selectedPlaylists.length >= 2 && selectedPlaylists.length <= 3) {
     showDuplicatesButton.classList.remove('disabled');
     showDuplicatesButton.classList.add('enabled');
     showDuplicatesButton.disabled = false; // Enable the button
   } else {
     showDuplicatesButton.classList.remove('enabled');
     showDuplicatesButton.classList.add('disabled');
-    showDuplicatesButton.disabled = true; // Disable the button  
+    showDuplicatesButton.disabled = true; // Disable the button
   }
 }
 
+// Function to update the "Remove Duplicates" button's state
+function updateRemoveDuplicatesButtonState() {
+  const removeDuplicatesButton = document.getElementById('remove-duplicates');
+
+  if (selectedDuplicates.length >= 1) {
+    removeDuplicatesButton.classList.remove('disabled');
+    removeDuplicatesButton.classList.add('enabled');
+    removeDuplicatesButton.disabled = false; // Enable the button
+  } else {
+    removeDuplicatesButton.classList.remove('enabled');
+    removeDuplicatesButton.classList.add('disabled');
+    removeDuplicatesButton.disabled = true; // Disable the button
+  }
+}
 
 function fetchTracks(playlistId) {
   return fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
@@ -91,24 +107,23 @@ function fetchTracks(playlistId) {
       'Authorization': 'Bearer ' + accessToken
     }
   })
-  .then(response => response.json())
-  .then(data => data.items.map(item => item.track)) // We're only interested in the actual track objects
-  .catch(error => console.error('Error:', error));
+    .then(response => response.json())
+    .then(data => data.items.map(item => item.track)) // We're only interested in the actual track objects
+    .catch(error => console.error('Error:', error));
 }
 
-
-document.getElementById("login-button").addEventListener("click", function() {
+document.getElementById("login-button").addEventListener("click", function () {
   // Spotify authentication process
   // Replace YOUR_CLIENT_ID with your actual Spotify API client ID
-  
+
   // Define the Spotify authorization URL
   const scope = 'playlist-read-private';
   const authUrl = `https://accounts.spotify.com/authorize?client_id=9fdba1a5111447ebad9b2213859f814a&response_type=token&scope=${encodeURIComponent(scope)}&redirect_uri=http://localhost:5500/redirect.html`;
-  
+
   // Calculate the window size based on the content
   const windowWidth = Math.min(window.innerWidth - 100, 500); // Adjust the subtracted value as needed
   const windowHeight = Math.min(window.innerHeight - 100, 800); // Adjust the subtracted value as needed
-  
+
   // Open the authentication window with the calculated size
   const authWindow = window.open(authUrl, "_blank", `width=${windowWidth},height=${windowHeight}`);
 
@@ -121,38 +136,37 @@ document.getElementById("login-button").addEventListener("click", function() {
     const playlistsSection = document.getElementById('playlists');
     const loginButton = document.getElementById('login-button');
     const readmeSection = document.querySelector('.readme');
-    const signOutButton = document.getElementById('sign-out-button'); // It should be 'sign-out-button' not 'sign out-button'
-    const showDuplicatesButton = document.getElementById('show-duplicates'); // Get the reference to the 'Show Duplicates' button
+    const signOutButton = document.getElementById('sign-out-button');
+    const showDuplicatesButton = document.getElementById('show-duplicates');
 
     // Perform further actions with the access token
     // Add your code here to interact with the Spotify API using the access token
-      
+
     // Remove the event listener
     window.removeEventListener("message", handleCallback);
-        
+
     // Call fetchPlaylists after successful authentication
     fetchPlaylists(offset);
 
-    // Show the 'Playlists' section, the 'Show Duplicates' button and the sign out button, and hide the login button and the README section
+    // Show the 'Playlists' section, the 'Show Duplicates' button, and the sign out button, and hide the login button and the README section
     playlistsSection.style.display = 'block';
     loginButton.style.display = 'none';
     readmeSection.style.display = 'none';
     signOutButton.style.display = 'block';
-    showDuplicatesButton.style.display = 'block'; // Display the 'Show Duplicates' button
-};
+    showDuplicatesButton.style.display = 'block';
+  };
 
-  
   // Listen for the callback message from the authentication window
   window.addEventListener("message", handleCallback);
 });
 
-document.getElementById("load-more").addEventListener("click", function() {
+document.getElementById("load-more").addEventListener("click", function () {
   // Increase the offset by the limit and fetch the next batch of playlists
   offset += limit;
   fetchPlaylists(offset);
 });
 
-document.getElementById('show-duplicates').addEventListener('click', function() {
+document.getElementById('show-duplicates').addEventListener('click', function () {
   const playlist1Id = selectedPlaylists[0];
   const playlist2Id = selectedPlaylists[1];
 
@@ -169,6 +183,7 @@ document.getElementById('show-duplicates').addEventListener('click', function() 
       duplicates.forEach(track => {
         html += `
         <div>
+          <input type="checkbox" id="${track.id}" value="${track.id}" name="duplicate">
           <img src="${track.album.images[2].url}" alt="${track.name} cover">
           <strong>${track.name}</strong> by ${track.artists[0].name}
         </div>`;
@@ -187,6 +202,64 @@ document.getElementById('show-duplicates').addEventListener('click', function() 
       // Change 'Show Duplicates' button to 'Remove Duplicates'
       const showDuplicatesButton = document.getElementById('show-duplicates');
       showDuplicatesButton.innerHTML = 'Remove Duplicates';
-      showDuplicatesButton.id = 'remove-duplicates';  // change the id of the button
+      showDuplicatesButton.id = 'remove-duplicates'; // Change the id of the button
+
+      // Hide the Load More button
+      const loadMoreButton = document.getElementById('load-more');
+      loadMoreButton.style.display = 'none';
+
+      // Get the checkboxes for duplicates and add event listeners
+      const duplicateCheckboxes = document.getElementsByName('duplicate');
+      duplicateCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', (event) => {
+          if (event.target.checked) {
+            selectedDuplicates.push(event.target.value);
+          } else {
+            const index = selectedDuplicates.indexOf(event.target.value);
+            if (index > -1) {
+              selectedDuplicates.splice(index, 1);
+            }
+          }
+
+          // Call updateRemoveDuplicatesButtonState after a checkbox's state changes
+          updateRemoveDuplicatesButtonState();
+        });
+      });
     });
+});
+
+document.getElementById('remove-duplicates').addEventListener('click', function () {
+  // Get the selected duplicate track IDs
+  const selectedDuplicateTrackIds = selectedDuplicates;
+
+  // Perform the remove duplicates action with the selected track IDs
+  // Add your code here to remove duplicates using the Spotify API
+
+  // Clear the selected duplicates array
+  selectedDuplicates = [];
+
+  // Update the button state
+  updateRemoveDuplicatesButtonState();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Update the button state initially
+  updateRemoveDuplicatesButtonState();
+});
+
+// Add event listener to checkboxes in the duplicates section
+document.querySelectorAll('#duplicates input[name="duplicate"]').forEach(checkbox => {
+  checkbox.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      selectedDuplicates.push(event.target.value);
+    } else {
+      const index = selectedDuplicates.indexOf(event.target.value);
+      if (index > -1) {
+        selectedDuplicates.splice(index, 1);
+      }
+    }
+
+    // Call updateRemoveDuplicatesButtonState after a checkbox's state changes
+    updateRemoveDuplicatesButtonState();
+  });
 });
