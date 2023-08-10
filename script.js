5e2a73993d1e1d6cc793501e1c3798cb415df43b
Morgan Bosc <morganbosc.music@gmail.com>
2023-08-10 15:03:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function toggleButtonState(buttonId, enable) {
 // Function to update the "Show Duplicates" button's state
 function updateDuplicatesButtonState() {
   const showDuplicatesButton = document.getElementById('show-duplicates');
-  const enableButton = selectedPlaylists.length >= 2 && selectedPlaylists.length <= 3;
+  const enableButton = selectedPlaylists.length >= 1 && selectedPlaylists.length <= 2;
   toggleButtonState('show-duplicates', enableButton);
 }
 
